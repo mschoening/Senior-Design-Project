77867ee327c54d5c225ce047068d83a18040528e
mschoening <mschoeni@umich.edu>
2019-03-28 14:10:58 -0400

    top spheres layer and minor comments cleaning
    added a layer with top spheres. Also cleaned up some of the comments because they were bugging me.

--- 3D Map Project/Scripts/Script.js
+++ 3D Map Project/Scripts/Script.js
@@ -46,6 +46,9 @@ require([
 		function setYear(value){
 			MosquesLayer.renderer = generateRender(value);
 			connLayer.renderer = generateConnRender(value);
+			topShpheresLayer.renderer = generateTopShpereRender(value);
+			topShpheresLayer.elevationInfo = generateSphereHeight(value);
+			
 		}
 		
 		
@@ -145,10 +148,11 @@ require([
 			MosquesLayer.definitionExpression = CountyExpressionBuilder();
 		}
 		
-		
-      /**************************************************
-       * Renderer for symbolizing mosques on time axis
-       **************************************************/
+		/***********************************************************
+		 *Functions for rendering the different layers on the map
+		 **********************************************************/
+      
+		//Renderer for symbolizing mosques on time axis
 		function generateRender(year){
 			//var testOutput = document.getElementById("debugTxt");
 			
@@ -230,9 +234,84 @@ require([
 			}
 		};
 		
-		/***************************************
-		* Function for generating connections. *
-		****************************************/
+		//Function for generating the top spheres. 
+		
+		function generateTopShpereRender(year){
+			var fieldPkr = "When($feature.CloseDate <= " + year + ",1, $feature.OpenDate <= " + year +" && $feature.CloseDate > " + year + ",2,  $feature.OpenDate > " + year + ", 3, 4)"; // need to change this to CDate and ODate when data is updated 		
+			
+			//testOutput.innerHTML=String(growExp2);
+			return{
+				type: "simple", // autocasts as new SimpleRenderer()
+				symbol: {
+					type: "point-3d", // autocasts as new PointSymbol3D()
+					symbolLayers: [{
+						type: "object", // autocasts as new ObjectSymbol3DLayer()
+						resource: {
+							primitive: "sphere"
+						},			
+						anchor: "bottom", // Dwight: Was 'Bottom', changed to 'bottom' (Threw error beforehand)
+						width: 160
+					}]
+				},
+				//signifies variables that can be changed via data
+				visualVariables: [{
+					type: "color",
+					valueExpression: fieldPkr,
+					stops: [{
+						value: 3,
+						color: {
+							r: 245,
+							g: 41,
+							b: 235,
+							a: 0
+						}, 
+					},
+					{
+						value: 2,
+						color: {
+							r: 245,
+							g: 41,
+							b: 235,
+							a: 1
+						},
+					},
+					{
+						value: 1,
+						color: {
+							r: 23,
+							g: 173,
+							b: 178,
+							a: 0.5
+						},
+					},
+					{
+						value: 4,
+						color: {
+							r: 255,
+							g: 102,
+							b: 0,
+							a: 1
+						},
+					}]
+					
+				}]
+			}
+		};
+		
+		//Function to generate top shpere height
+		
+		function generateSphereHeight(year){
+			genHeight = "IIf (" + year + ">= $feature.CloseDate, $feature.z + (($feature.Closedate-$feature.OpenDate) * 10) - 5, $feature.z + (("+ year + "-$feature.OpenDate)*10) - 5)";
+			return{
+				
+				mode: "relative-to-ground",
+				featureExpressionInfo:{
+					expression: genHeight
+				}	
+			}
+		};
+		
+		//Function for generating connections layer.
 		function generateConnRender(year){
 			
 			return {
@@ -271,8 +350,9 @@ require([
 				}]
 				
 			}	
-		}
+		};
 		
+		//Renderer for the layer showing mosques on the surface.
 		var mosquesSurfaceRenderer = {
 			type: "simple", // autocasts as new SimpleRenderer()
 			symbol: {
@@ -290,6 +370,7 @@ require([
 			}
 		};
 		
+		//Renderer for the layer showing the county highlights. 
 		var countiesRenderer = {
 			type: "simple", // autocasts as new SimpleRenderer()
 			symbol: {
@@ -302,7 +383,9 @@ require([
 					0.5
 				],
 			}	
-		}
+		};
+		
+		//Renderer for the layer showing the city highlights 
 		var citiesRenderer = {
 			type: "simple", // autocasts as new SimpleRenderer()
 			symbol: {
@@ -315,7 +398,7 @@ require([
 					0.5
 				],
 			}	
-		}
+		};
 	  
 	  /**************************************************
        * mosque popup template
@@ -329,9 +412,10 @@ require([
 		   "<a href={Link} rel='modal:open'>Click Here To Learn More</a>"
 		 };
 
-      /**************************************************
-       * Layers depicting mosques
-       **************************************************/
+
+      /***********************************************************************
+       * Code that sets up the layers with the previously defined renderers. 
+       **********************************************************************/
 
       // Layer for depicting mosques on time axis
 		var MosquesLayer = new FeatureLayer({
@@ -347,7 +431,16 @@ require([
 				}
 			},
 			unit: "meters"
-		});   
+		});
+		
+		//Layer for depecting top spheres. 
+		var topShpheresLayer = new FeatureLayer({
+			url: mosquesUrl,
+			definitionExpression: "",
+			outFields: ["*"],
+			popupTemplate: mosqueTemplate,
+			returnZ: true
+		});
 		
 		// Layer for depecting county highlighting. 
 		var countiesLayer = new FeatureLayer({
@@ -399,7 +492,7 @@ require([
        ********************************************************************/
 		var map = new Map({
 			basemap: "dark-gray",
-			layers: [MosquesLayer, countiesLayer, citiesLayer, connLayer, mosquesSurfaceLayer],
+			layers: [MosquesLayer, countiesLayer, citiesLayer, connLayer, mosquesSurfaceLayer,topShpheresLayer],
 			ground: {
 				navigationConstraint: {
 					type: "stay-above" //Dwight: Changed 'stayAbove' to 'stay-above' (threw error beforehand)
@@ -469,13 +562,13 @@ function openTab(evt, tName){
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i = 0; i < tabcontent.length; i++) {
 		tabcontent[i].style.display = "none";
-  }
+	}
 
   // Get all elements with class="tablinks" and remove the class "active"
 	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+	}
 
   // Show the current tab, and add an "active" class to the button that opened the tab
 	document.getElementById(tName).style.display = "block";
